c5a8c7cbe06a3bfeecde1a0996348f597a068287
augustinaweb <a.radziunaite@gmail.com>
2021-07-27 18:28:05 +0300

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,14 @@
-import React from 'react';
-import './App.css';
-import { layout } from './helper';
+import React from "react";
+import "./App.css";
+import { layout, setItemClass, movePacman } from "./helper";
 
 const App = () => {
 	const [currentPacmanIndex, setCurrentPacmanIndex] = React.useState(347);
-	const setItemClass = (item: number, i: number) => {
-		if (i === currentPacmanIndex) {
-			return 'pac-man';
-		}
-		if (item === 0) {
-			return 'empty';
-		}
-		if (item === 1) {
-			return 'wall';
-		}
-		if (item === 2) {
-			return 'pac-dot';
-		}
-		if (item === 3) {
-			return 'ghost-lair';
-		}
-		if (item === 4) {
-			return 'power-pellet';
-		}
-	};
 
-	const handleKeyDown = (event: React.KeyboardEvent) => {
-		console.log(event.key);
-		//switch (event) {
-		//	case '37':
-		//		setCurrentPacmanIndex(currentPacmanIndex + 1);
-		//}
-	};
+	document.addEventListener("keyup", (e) =>
+		movePacman(e, currentPacmanIndex, setCurrentPacmanIndex)
+	);
 
-	document.addEventListener('keydown', () => handleKeyDown);
 	return (
 		<div className="game">
 			<h1>
@@ -42,7 +17,14 @@ const App = () => {
 			<div className="grid">
 				{layout &&
 					layout.map((item, i) => (
-						<div key={i} className={setItemClass(item, i)}></div>
+						<div
+							key={i}
+							className={setItemClass(
+								item,
+								i,
+								currentPacmanIndex
+							)}
+						></div>
 					))}
 			</div>
 		</div>
--- src/helper.ts
+++ src/helper.ts
@@ -1,3 +1,57 @@
+export const movePacman = (
+	e: any,
+	currentPacmanIndex: number,
+	setCurrentPacmanIndex: (arg: number) => void
+) => {
+	switch (e.key) {
+		case "ArrowUp":
+			e.preventDefault();
+			if (currentPacmanIndex - 24 >= 0) {
+				setCurrentPacmanIndex(currentPacmanIndex - 24);
+			}
+			break;
+		case "ArrowDown":
+			e.preventDefault();
+			if (currentPacmanIndex + 24 < 576) {
+				setCurrentPacmanIndex(currentPacmanIndex + 24);
+			}
+			break;
+		case "ArrowLeft":
+			e.preventDefault();
+			if (currentPacmanIndex > 0 && currentPacmanIndex / 24 !== 1) {
+				setCurrentPacmanIndex(currentPacmanIndex - 1);
+			}
+			break;
+		case "ArrowRight":
+			e.preventDefault();
+			if (currentPacmanIndex < 576 && currentPacmanIndex / 23 !== 1) {
+				setCurrentPacmanIndex(currentPacmanIndex + 1);
+			}
+			break;
+	}
+};
+
+export const setItemClass = (item: number, i: number, currentIndex: number) => {
+	if (i === currentIndex) {
+		return "pac-man";
+	}
+	if (item === 0) {
+		return "empty";
+	}
+	if (item === 1) {
+		return "wall";
+	}
+	if (item === 2) {
+		return "pac-dot";
+	}
+	if (item === 3) {
+		return "ghost-lair";
+	}
+	if (item === 4) {
+		return "power-pellet";
+	}
+};
+
 export const layout = [
 	1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
 	1, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1,
