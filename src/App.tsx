cd5530fbd4419e3eef49765c5698d29d9b8a1d4e
augustinaweb <a.radziunaite@gmail.com>
2021-07-27 17:18:58 +0300

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { layout } from './helper';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [currentPacmanIndex, setCurrentPacmanIndex] = React.useState(347);
+	const setItemClass = (item: number, i: number) => {
+		if (i === currentPacmanIndex) {
+			return 'pac-man';
+		}
+		if (item === 0) {
+			return 'empty';
+		}
+		if (item === 1) {
+			return 'wall';
+		}
+		if (item === 2) {
+			return 'pac-dot';
+		}
+		if (item === 3) {
+			return 'ghost-lair';
+		}
+		if (item === 4) {
+			return 'power-pellet';
+		}
+	};
+
+	const handleKeyDown = (event: React.KeyboardEvent) => {
+		console.log(event.key);
+		//switch (event) {
+		//	case '37':
+		//		setCurrentPacmanIndex(currentPacmanIndex + 1);
+		//}
+	};
+
+	document.addEventListener('keydown', () => handleKeyDown);
+	return (
+		<div className="game">
+			<h1>
+				Score:<span id="score">0</span>
+			</h1>
+			<div className="grid">
+				{layout &&
+					layout.map((item, i) => (
+						<div key={i} className={setItemClass(item, i)}></div>
+					))}
+			</div>
+		</div>
+	);
+};
 
 export default App;
